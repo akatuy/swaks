54d558203383f68067176ad134eb2bd51b888528
John Jetmore <jj33@pobox.com>
2020-12-15 17:42:22 -0500

    enable proxy testing w/ exim test server

--- testing/mta/exim-config/start.sh
+++ testing/mta/exim-config/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo /home/jetmore/Documents/git/swaks/testing/mta/exim-install/bin/exim -d -bd -oX 1025
+# can pass args directly through to exim, like:
+# ./start.sh -DPROXY=true
+
+# Turn proxy on:
+# -DPROXY=true
+
+sudo /home/jetmore/Documents/git/swaks/testing/mta/exim-install/bin/exim -d -bd -oX 1025 $*
